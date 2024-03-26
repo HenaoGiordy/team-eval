72ff038bfd458077d165f49545997017e1f107a0
Giordy Pavel Henao Montes <giordy.henao@correounivalle.edu.co>
2024-03-26 13:52:27 -0500

    cambios en la entidad de usuario

--- app/signals.py
+++ app/signals.py
@@ -1,45 +1,46 @@
-from .models import Administrador, Estudiante, Profesor, PerfilEstudiante, PerfilProfesor
+from .models import User, PerfilEstudiante, PerfilProfesor
 from django.contrib.auth.models import Group
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
-@receiver(post_save, sender=Administrador)
-def asignar_grupo_administrador(sender, instance, created, *args, **kwards):
-    if created:
-        try:
-            grupo = Group.objects.get(name='administradores')
-        except Group.DoesNotExist:
-            grupo = Group.objects.create(name='administradores')
-        instance.groups.add(grupo)
+@receiver(post_save, sender=User)
+def asignar_grupo_administrador(sender, instance, *args, **kwards):
+    if instance.role != "":
+        if  instance.role=='ADMIN':
+            try:
+                grupo = Group.objects.get(name='administradores')
+            except Group.DoesNotExist:
+                grupo = Group.objects.create(name='administradores')
+                instance.groups.add(grupo)
+    
+        if instance.role=='ESTUDIANTE':
+            try:
+                grupo = Group.objects.get(name='Estudiantes')
+            except Group.DoesNotExist:
+                grupo = Group.objects.create(name='Estudiantes')
+            instance.groups.add(grupo)
+    
+        if instance.role=='PROFESOR':
+            try:
+                grupo = Group.objects.get(name='Profesores')
+            except Group.DoesNotExist:
+                grupo = Group.objects.create(name='Profesores')
+            instance.groups.add(grupo)
+    
 
-@receiver(post_save, sender=Estudiante)
-def asignar_grupo_estudiante(sender, instance, created, *args, **kwards):
-    if created:
-        try:
-            grupo = Group.objects.get(name='estudiantes')
-        except Group.DoesNotExist:
-            grupo = Group.objects.create(name='estudiantes')
-        instance.groups.add(grupo)
-
-@receiver(post_save, sender=Estudiante)
+@receiver(post_save, sender=User)
 def crear_perfil(sender, instance, created, *args, **kwards):
-    if created and instance.role == 'Estudiante':
-        PerfilEstudiante.objects.create(user=instance,nombre=instance.username)
+    if instance.role != "":
+        if instance.role == 'ESTUDIANTE':
+            if not PerfilEstudiante.pk: 
+                PerfilEstudiante.objects.create(user=instance,nombre=instance.username)
+        if instance.role == 'PROFESOR':
+            if not PerfilProfesor.pk:
+                PerfilProfesor.objects.create(user=instance,nombre=instance.username)
+    
 
 
-@receiver(post_save, sender=Profesor)
-def asignar_grupo_profesor(sender, instance, created, *args, **kwards):
-    if created:
-        try:
-            grupo = Group.objects.get(name='profesores')
-        except Group.DoesNotExist:
-            grupo = Group.objects.create(name='profesores')
-        instance.groups.add(grupo)
-        
-@receiver(post_save, sender=Profesor)
-def crear_perfil(sender, instance, created, *args, **kwards):
-    if created and instance.role == 'Profesor':
-        PerfilEstudiante.objects.create(user=instance,nombre=instance.username)
+    
 
 
         
