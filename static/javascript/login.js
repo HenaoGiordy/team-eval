b4605dd03d72c93840fc1d7856b9712cdf67f45b
Giordy Pavel Henao Montes <giordy.henao@correounivalle.edu.co>
2024-03-28 19:06:44 -0500

    refactoring login

---
+++ static/javascript/login.js
@@ -0,0 +1,26 @@
+let usernameInput = document.getElementById("username")
+let passwordInput = document.getElementById("password")
+
+
+usernameInput.addEventListener("focus", (evento)=>{
+    label = document.getElementById("username-label")
+    label.style.color = "#0a58ca"
+})
+
+usernameInput.addEventListener("blur", (evento)=>{
+    label = document.getElementById("username-label")
+    label.style.color = "black"
+})
+
+passwordInput.addEventListener("focus", (evento)=>{
+    
+    label = document.getElementById("password-label")
+    label.style.color = "#0a58ca"
+})
+
+passwordInput.addEventListener("blur", (evento)=>{
+    
+    label = document.getElementById("password-label")
+    label.style.color = "black"
+})
+
