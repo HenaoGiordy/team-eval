814c5866bbfb1a639e28f0c53d85e494bbc7009c
Giordy Pavel Henao Montes <giordy.henao@correounivalle.edu.co>
2024-04-16 13:46:23 -0500

    enrutamiento de estudiantes a curso

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns= [
     path('', views.login_register, name="login"),
     path('estudiante/', views.estudiante, name="estudiante"),
-    path('estudiante/curso/<str:cursoid>', views.estudiante_curso, name="estudiante"),
+    path('estudiante/curso/<str:cursoid>', views.estudiante_curso, name="estudiante_curso"),
     path('profesor/', views.profesor, name="profesor")
 ]
