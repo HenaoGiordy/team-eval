69e50145f10cc0a0c1f20a6c82de10ab7ee00e4a
Giordy Henao <Henaogiordy@gmail.com>
2024-06-12 17:39:06 -0500

    Actualizacion correo app

--- TeamEval/settings.py
+++ TeamEval/settings.py
@@ -145,5 +145,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'giordy.henao@correounivalle.edu.co'
-EMAIL_HOST_PASSWORD = 'qnrc aecr hzby vtcr '
+EMAIL_HOST_USER = 'team.eval2024@gmail.com'
+EMAIL_HOST_PASSWORD = 'nhii vbyp dewm vbsb'
