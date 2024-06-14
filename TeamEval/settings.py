1130c937f5c5405d2c42c00b58d1ac53fb372802
MiniadamGod <bayron.sandoval@correounivalle.edu.co>
2024-06-13 19:12:50 -0500

    Configuración de la base de datos

--- TeamEval/settings.py
+++ TeamEval/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'TeamEval.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teamEval_database',  
+        'USER': 'postgres',       
+        'PASSWORD': 'Postgres*',  
+        'HOST': 'localhost',     
+        'PORT': '5432',          
     }
 }
+
 
 
 # Password validation
